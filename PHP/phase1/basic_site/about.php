8a30f8791e865accf9fc28eaab0c66fded9940a9
dipherent1 <binyammulat244@gmail.com>
2025-09-23 14:26:43 +0300

    Add header, footer, about, and index pages with initial content

---
+++ PHP/phase1/basic_site/about.php
@@ -0,0 +1,7 @@
+<?php require 'partials/header.php'; ?>
+
+    <h2>About Our Company</h2>
+    <p>This is the content for the about page. We are a great company that does amazing things.</p>
+    <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.</p>
+
+<?php require 'partials/footer.php'; ?>
---
+++ PHP/phase1/basic_site/partials/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+    <p>&copy; <?= date("Y"); ?>My Simple Website. All rights reserverd</p>
+</footer>
+</body>
+</html>
