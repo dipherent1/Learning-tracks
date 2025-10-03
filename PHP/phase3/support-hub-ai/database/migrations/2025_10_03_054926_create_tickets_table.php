bcd6c55ca451e3451646f524abc6cae540d8be51
dipherent1 <binyammulat244@gmail.com>
2025-10-03 10:29:49 +0300

    feat: generate models and their migration

---
+++ PHP/phase3/support-hub-ai/database/migrations/2025_10_03_054926_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('department_id')->constrained()->onDelete('cascade');
+        $table->foreignId('agent_id')->nullable()->constrained('users')->onDelete('set null');
+
+        $table->string('title');
+        $table->text('content');
+        $table->string('status')->default('open'); // e.g., open, in_progress, closed
+        $table->string('priority')->nullable(); // e.g., low, medium, high (for AI)
+        $table->string('category')->nullable(); // e.g., billing, technical (for AI)
+        $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
